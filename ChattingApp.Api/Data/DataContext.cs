15790ed351e52f1bf5b8aa0fb1836a3415822c31
Chaitanya Penmetsa <CPenmetsa@americancampus.com>
2018-05-26 08:35:14 -0500

    Initial Commit

---
+++ ChattingApp.Api/Data/DataContext.cs
@@ -0,0 +1,15 @@
+using ChattingApp.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChattingApp.Api.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Value> Values {get; set;}
+    }
+}

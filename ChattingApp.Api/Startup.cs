b38d717e81008324d82c5af105ed89c144c502c9
Chaitanya Penmetsa <CPenmetsa@americancampus.com>
2018-05-28 09:20:13 -0500

    Initial Commit for chattingapp web

--- ChattingApp.Api/Startup.cs
+++ ChattingApp.Api/Startup.cs
@@ -27,6 +27,7 @@ namespace ChattingApp.Api
         {
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc();
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,6 +38,7 @@ namespace ChattingApp.Api
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin().AllowCredentials());
             app.UseMvc();
         }
     }
